eebd48d0f0e90049018af4c7c0dbca92300f55c7
missstech <njerikimundia@gmail.com>
2023-06-15 10:01:14 -0700

    101.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that prints returns 1 and has no warnings
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
 write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
